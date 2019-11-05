7a34489387924344ad9edfc7affcd7d49b1fe903
Vladimir Filipovic <vladofilipovic@hotmail.com>
2019-11-05 15:00:59 +0100

    popravio

---
+++ predavanja/primeri-js/p06-napredni-objekti/js-06-07b-prototipsko-nasledjivanje-prototip.js
@@ -0,0 +1,64 @@
+let zecPrototip = {
+    tip: "непознат",
+
+    boja: "непознатa",
+
+    predstaviSe: function () {
+        console.log("Зец: " + this.tip + " боја: " + this.boja + "." + "'\n");
+    },
+
+    govori: function (tekst) {
+        console.log("Овај зец " + this.tip + " боје " + this.boja
+            + " каже '" + tekst + "'" + "'\n");
+    }
+};
+
+let zec = Object.create(zecPrototip);
+zec.predstaviSe();
+// >>> Зец: непознат боја: непознатa.
+zec.govori("Ко сам ја?");
+// >>> Овај зец непознат боје непознатa каже 'Ко сам ја?'
+
+
+function ZecIzFikcije(tip, boja, ime,
+    imeKreatora, prezimeKreatora, delo,
+    uzrecica) {
+    this.ime = ime;
+    this.kreator = { "ime": imeKreatora, "prezime": prezimeKreatora };
+    this.delo = delo;
+    this.uzrecica = uzrecica;
+}
+
+ZecIzFikcije.prototype = Object.create(zecPrototip);
+
+ZecIzFikcije.prototype.predstaviSe = function () {
+    console.log("Зец: " + this.tip + ", боја: " + this.boja + ", име: " + this.ime + "\n"
+        + "креатор: " + this.kreator.ime + " " + this.kreator.prezime + "\n"
+        + "дело: " + this.delo + "\n"
+        + "узречица: '" + this.uzrecica + "'\n");
+}
+
+let duskoDugousko = Object.create(ZecIzFikcije.prototype);
+duskoDugousko.tip = "паметан";
+duskoDugousko.boja = "сива";
+duskoDugousko.ime = "Душко Дугоушко";
+duskoDugousko.kreator = { "ime": "Tex", "prezime": "Avery" };
+duskoDugousko.delo = "A Wild Hare";
+duskoDugousko.uzrecica = "Шефе, који ти је враг?";
+duskoDugousko.predstaviSe();
+// >>> Зец: паметан, боја: сива, име: Душко Дугоушко
+// >>> креатор: Tex Avery
+// >>> дело: A Wild Hare
+// >>> узречица: 'Шефе, који ти је враг?'
+let plaviZec = Object.create(ZecIzFikcije.prototype);
+plaviZec.tip = "веома паметан";
+plaviZec.boja = "плава";
+plaviZec.ime =  "Плави ѕец";
+plaviZec.kreator = { "ime": "Душко", "prezime": "Радовић" };
+plaviZec.delo = "Плави зец";
+plaviZec.uzrecica = "Плави, зец, чудни зец, једини на свету.";
+plaviZec.predstaviSe();
+// >>> Зец: веома паметан, боја: плава, име: Плави ѕец
+// >>> креатор: Душко Радовић
+// >>> дело: Плави зец
+// >>> узречица: 'Плави, зец, чудни зец, једини на свету.'
---
+++ predavanja/primeri-js/p06-napredni-objekti/js-06-07c-prototipsko-nasledjivanje-konstruktor.js
@@ -0,0 +1,65 @@
+function Zec(tip = "непознат", boja = "непознатa") {
+
+    this.tip = tip;
+    this.boja = boja;
+}
+
+Zec.prototype.predstaviSe = function () {
+    console.log("Зец: " + this.tip + " боја: " + this.boja + "."+ "'\n");
+}
+
+Zec.prototype.govori = function (tekst) {
+    console.log("Овај зец " + this.tip + " боје " + this.boja
+        + " каже '" + tekst + "'"+ "'\n");
+}
+
+let zec = new Zec();
+zec.predstaviSe();
+// >>> Зец: непознат боја: непознатa.
+zec.govori("Ко сам ја?");
+// >>> Овај зец непознат боје непознатa каже 'Ко сам ја?'
+
+function ZecIzFikcije(tip, boja, ime,
+    imeKreatora, prezimeKreatora, delo,
+    uzrecica) {
+    Zec.call(this, tip, boja);
+    this.ime = ime;
+    this.kreator = { "ime": imeKreatora, "prezime": prezimeKreatora };
+    this.delo = delo;
+    this.uzrecica = uzrecica;
+
+    this.predstaviSe = function () {
+        console.log("Зец: " + this.tip + ", боја: " + this.boja + ", име: " + this.ime + "\n"
+            + "креатор: " + this.kreator.ime + " " + this.kreator.prezime + "\n"
+            + "дело: " + this.delo + "\n"
+            + "узречица: '" + this.uzrecica + "'\n");
+    }
+}
+
+ZecIzFikcije.prototype = Object.create(Zec.prototype);
+
+ZecIzFikcije.prototype.skoci = function(){
+    console.log("Скок, скок, скок \n");
+};
+
+let duskoDugousko = new ZecIzFikcije("паметан", "сива", "Душко Дугоушко",
+    "Tex", "Avery", "A Wild Hare", "Шефе, који ти је враг?");
+duskoDugousko.predstaviSe();
+// >>> Зец: паметан, боја: сива, име: Душко Дугоушко
+// >>> креатор: Tex Avery
+// >>> дело: A Wild Hare
+// >>> узречица: 'Шефе, који ти је враг?'
+duskoDugousko.govori(duskoDugousko.uzrecica);
+// >>> Овај зец паметан боје сива каже 'Шефе, који ти је враг?''
+duskoDugousko.skoci();
+// >>> Скок, скок, скок
+
+let plaviZec = new ZecIzFikcije("веома паметан", "плава", "Плави ѕец",
+    "Душко", "Радовић", "Плави зец", "Плави, зец, чудни зец, једини на свету.");
+plaviZec.predstaviSe();
+// >>> Зец: веома паметан, боја: плава, име: Плави ѕец
+// >>> креатор: Душко Радовић
+// >>> дело: Плави зец
+// >>> узречица: 'Плави, зец, чудни зец, једини на свету.'
+plaviZec.govori(plaviZec.uzrecica);
+// >>> Овај зец веома паметан боје плава каже 'Плави, зец, чудни зец, једини на свету.''
