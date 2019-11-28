c95afdc0264e2ee26c9b01785cbe2ec6c84dae36
Vladimir Filipovic <vladofilipovic@hotmail.com>
2019-11-28 16:34:29 +0100

    popravio tabelu

---
+++ predavanja/primeri-js/p01-vrednosti-tipovi-operatori/js-01-04-metodi-nad-niskama.js
@@ -0,0 +1,11 @@
+console.log("miki".charAt(3));
+
+console.log("miki".charCodeAt(3));
+
+console.log("miki".concat("maus").concat("mini").concat("maus"));
+console.log("miki".concat("maus", "mini", "maus"));
+console.log("".concat("miki","maus", "mini", "maus"));
+// ovo ne radi !
+//console.log(String.concat("miki","maus", "mini", "maus"));
+
+console.log(String.fromCharCode(106));
