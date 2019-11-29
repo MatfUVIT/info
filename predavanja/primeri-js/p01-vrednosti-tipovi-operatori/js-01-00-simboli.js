b53019c552bbffcb2564385006b94c21b0b3d84a
Vladimir Filipovic <vladofilipovic@hotmail.com>
2019-11-29 18:04:02 +0100

    typo

--- predavanja/primeri-js/p01-vrednosti-tipovi-operatori/js-01-00-simboli.js
+++ predavanja/primeri-js/p01-vrednosti-tipovi-operatori/js-01-00-simboli.js
@@ -1,12 +1,12 @@
 /* ћириличне речи могу бити 
 имена променљивих */
 const мићко = 42
-console.log(`мићко`)
+console.log('мићко')
 console.log(мићко)
 
 // Unicode знаци могу да 
 // буду имeна променљивих
 const \u1F601 = 42
-console.log(`\u1F601`)
+console.log('\u1F601')
 console.log(\u1F601)
 
