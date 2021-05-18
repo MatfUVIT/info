ec5210cb8fd60ee039e8f1b07bfe3270d0a825f8
djinx <key13043@gmail.com>
2021-05-18 20:19:46 +0200

    Sitna izmena

--- vezbe/knjiga/Poglavlja/Mongoose/Primeri/2/controllers/results.js
+++ vezbe/knjiga/Poglavlja/Mongoose/Primeri/2/controllers/results.js
@@ -3,7 +3,6 @@ const exam = require('../models/exam');
 
 module.exports.displayResults = async function(req, res, next) {
     let resultsObject = await exam.getResults();
-    console.log(req.body);
     return res.render('results.ejs', 
     {
         student: {username: req.body.username, password: req.body.password},
